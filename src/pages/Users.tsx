8eeccc2b519cdfecbc8671a038088a8af04d6dc0
Rob Fairclough <rob@silverchip.com>
2019-10-08 13:08:25 +0100

    make a start on codegen setup

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
+import { getIntrospectionQuery } from 'graphql';
+import { gql } from 'apollo-boost';
 import { getUsers } from '../graphql/queries/users';
 
 const Users: React.FC = (props) => {
-  const { loading, error, data } = useQuery(getUsers);
+  const { loading, error, data } = useQuery(gql`${getIntrospectionQuery()}`);
 
   console.log({ loading, error, data });
 
